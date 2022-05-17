45de2f76d71aa30d2aca62513eec2c67c90eca44
Gridfas <809418027@qq.com>
2022-05-17 23:46:24 +0800

    第一次提交代码

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,43 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import routes from './router'
+import { createRouter, createWebHistory } from 'vue-router'
+import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
 
-createApp(App).mount('#app')
+let app: any
+
+function render(props: any = {}) {
+  const { container, routerBase = '/app-vue' } = props
+  const router = createRouter({
+    routes,
+    history: createWebHistory(routerBase),
+  })
+
+  app = createApp(App)
+  app.use(router).mount(container || '#app')
+}
+
+if (qiankunWindow.__POWERED_BY_QIANKUN__) {
+  renderWithQiankun({
+    bootstrap() { },
+    mount(props) {
+        props.onGlobalStateChange = (state: any, prev: any) => {
+            console.log(state, prev)
+        }
+        render(props)
+    },
+    unmount() {
+        app.unmount()
+        app = null
+    },
+    update: function (props: any): void | Promise<void> {
+        throw new Error('Function not implemented.')
+    }
+})
+}
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render()
+
+  console.log('独立运行')
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import qiankun from 'vite-plugin-qiankun'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), qiankun('app-vue', { useDevMode: true })],
+  server: {
+    port: 7888,
+    open: true
+  }
 })
