84ce5ff44386facbcb5ef7544efb3c5439ed7e41
Gridfas <809418027@qq.com>
2022-05-20 23:25:39 +0800

    更新代码并提交

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,10 @@ import type { RouteRecordRaw } from 'vue-router'
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    redirect: 'home',
+  },
+  {
+    path: '/home',
     name: 'home',
     component: () => import('../views/home.vue')
   },
