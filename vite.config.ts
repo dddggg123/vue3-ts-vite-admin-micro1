7dbba4412b7cd85d3a6e4b878d9a4e2a222c364a
Gridfas <809418027@qq.com>
2022-05-21 14:20:32 +0800

    update code

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import qiankun from 'vite-plugin-qiankun'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), qiankun('micro1', { useDevMode: true })],
-  server: {
-    port: 6888,
-    open: true
-  },
-  base: './',
-  publicDir: '/micro1/'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname)
+  return {
+    plugins: [vue(), qiankun('micro1', { useDevMode: true })],
+    server: {
+      port: 6888,
+      open: true
+    },
+    base: env.VITE_APP_BASE
+  }
 })
