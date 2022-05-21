362cb340b4903eae6e9368fa038b32cd3250c9b2
Gridfas <809418027@qq.com>
2022-05-21 23:08:49 +0800

    更新代码

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,4 +1,15 @@
 <template>
-  <h1>子应用关于界面</h1>
-  <router-link to="/">返回</router-link>
+  <div class="app-two-about-container">
+    <h1>子应用关于界面</h1>
+    <router-link to="/">返回</router-link>
+  </div>
 </template>
+
+<style lang="scss">
+.app-two-about-container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 20px;
+}
+</style>
